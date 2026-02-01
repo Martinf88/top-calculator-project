b69817ef9f46be88d3545d67e2b58abc8e57dcfc
Martinf88 <143597591+Martinf88@users.noreply.github.com>
2026-02-01 21:07:06 +0100

    adds click events for operatorBtns and equalsBtn

--- script.js
+++ script.js
@@ -1,37 +1,63 @@
-const digits = document.querySelectorAll(".digit");
+const digitBtns = document.querySelectorAll(".digit");
 const display = document.querySelector(".display");
+const operatorBtns = document.querySelectorAll(".operator");
+const equalsBtn = document.querySelector(".equals");
 
 let num1 = "";
 let num2 = "";
 let operator = "";
 
-digits.forEach((digit) => {
+digitBtns.forEach((digit) => {
   digit.addEventListener("click", () => {
-    num1 += digit.innerHTML;
-    display.innerHTML = num1;
+    /* if (num1 === "" && digit.innerHTML === ".") {
+      display.innerHTML === "0.1";
+    }*/
+    /*TODO: fix so that if the display displays 0 and . is pressed it shows 0.1 */
+    if (operator === "") {
+      num1 += digit.innerHTML;
+      display.innerHTML = num1;
+    } else {
+      num2 += digit.innerHTML;
+      display.innerHTML = num1 + " " + operator + " " + num2;
+    }
   });
 });
 
-/*Create the functions that update one of your number variables when the calculator’s digit buttons are clicked. 
-Your calculator’s display should also update to reflect the value of that number variable.*/
+operatorBtns.forEach((op) => {
+  op.addEventListener("click", () => {
+    if (operator !== "") return;
+    operator = op.innerHTML;
+    display.innerHTML += " " + operator;
+  });
+});
+
+equalsBtn.addEventListener("click", () => {
+  const result = operate(operator, Number(num1), Number(num2));
+  display.innerHTML = result;
+  num1 = "";
+  num2 = "";
+  operator = "";
+});
 
 const add = (num1, num2) => {
-  return `add: ${num1 + num2}`;
+  return num1 + num2;
 };
 
 const subtract = (num1, num2) => {
-  return `subtract: ${num1 - num2}`;
+  return num1 - num2;
 };
 
 const multiply = (num1, num2) => {
-  return `multiply: ${num1 * num2}`;
+  return num1 * num2;
 };
 
 const divide = (num1, num2) => {
-  return `divide: ${num1 / num2}`;
+  return num1 / num2;
 };
 
 const operate = (operator, num1, num2) => {
+  if (operator === "") return num1;
+
   if (operator === "+") {
     return add(num1, num2);
   } else if (operator === "-") {
