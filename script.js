c1592b51b5688dc010c53452be1908952d73abfe
Martinf88 <143597591+Martinf88@users.noreply.github.com>
2026-02-01 18:34:42 +0100

    adds add, subtract, multiply and divide functions to script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+const add = (num1, num2) => {
+  return `add: ${num1 + num2}`;
+};
+
+const subtract = (num1, num2) => {
+  return `subtract: ${num1 - num2}`;
+};
+
+const multiply = (num1, num2) => {
+  return `multiply: ${num1 * num2}`;
+};
+
+const divide = (num1, num2) => {
+  return `divide: ${num1 / num2}`;
+};
+
+console.log(add(5, 2));
+console.log(subtract(5, 2));
+console.log(multiply(5, 2));
+console.log(divide(5, 2));
